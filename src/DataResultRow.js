069b6adb7798f3ab245a47ed9f3931bafd3a9a96
Glitch (reactdemo-search) <none>
2019-05-08 13:25:19 +0000

    👊🐆 Updated with Glitch

--- src/DataResultRow.js
+++ src/DataResultRow.js
@@ -8,7 +8,7 @@ class DataResultsRow extends PureComponent {
       <div
         className="component-data-result-row copy-to-clipboard"
       >
-        <span className="title">{this.props.dataItem.name} - {this.props.dataItem.time}</span>
+        <span className="title">{this.props.dataItem.name} - {this.props.dataItem.data}</span>
       </div>
     );
   }
